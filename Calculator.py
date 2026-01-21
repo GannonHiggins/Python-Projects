66c577d6b92090982eb0861f28898e31e4db05a4
GannonHiggins <gannonhiggins@gmail.com>
2026-01-21 10:41:59 -0700

    added basic functions to the calculator

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,32 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Calculator")
-root.geometry("300x400")
-
-label = tk.Label(root, text = "Hello, World!")
-label.pack(pady=20)
+from tkinter import *
 
 
-root.mainloop()
+expr = "" #global expression variable
+
+def button_click(key):
+    global expr
+    expr += str(key)
+    display.set(expr)
+
+def equal():
+    global expr
+    try:
+        resault = str(eval(expr))
+        display.set(resault)
+        expr = ""
+    except:
+        display.set("Error")
+        expr = ""
+
+def clear():
+    global expr
+    expr = ""
+    display.set("")
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Calculator")
+    root.geometry("600x700")
+
+
+    root.mainloop()

33b5650d32b6f2ca192da74fb00356cb54bb8c59
GannonHiggins <gannonhiggins@gmail.com>
2026-01-21 11:32:42 -0700

    Implemented player movement and rendering in Snake game, including player position initialization and frame time calculation.

--- Snake.py
+++ Snake.py
@@ -6,7 +6,9 @@ pg.init()
 screen = pg.display.set_mode((1280,720)) #set the screen size
 clock = pg.time.Clock() #set the clock
 running = True
+dt = 0
 
+player_pos = pg.Vector2(screen.get_width()/2, screen.get_height()/2) #set the player position to the center of the screen
 
 while running:
     for event in pg.event.get():
@@ -14,9 +16,20 @@ while running:
             running = False
     
     screen.fill("black")
+    pg.draw.circle(screen, "white", player_pos, 15)
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pg.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pg.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pg.K_d]:
+        player_pos.x += 300 * dt
 
     #RENDER GAME HERE
     pg.display.flip()
-    clock.tick(60) #60 FPS
+    dt = clock.tick(60) / 1000 #60 FPS
 
 pg.quit()

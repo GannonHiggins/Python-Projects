4f2631a027755b7c13b80572dcbe0911f3daf043
GannonHiggins <gannonhiggins@gmail.com>
2026-01-21 10:26:14 -0700

    initiated calculator.py and added function to display GUI window

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("300x400")
+
+label = tk.Label(root, text = "Hello, World!")
+label.pack(pady=20)
+
+
+root.mainloop()
